f2a00dea167315c348833ccd4bc84e40f4675b87
Serhii Shepel <9946053+sshepel@users.noreply.github.com>
2019-03-03 14:04:05 +0200

    Add Docker manifest files for Skymind CI

--- hadoop-docker/historyserver/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker build -t skymindio-docker-dl4j-hadoop-historyserver.bintray.io/skymindio/dl4j-hadoop-historyserver .
--- hadoop-docker/runbootstrap.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-/opt/startssh.sh && /etc/bootstrap.sh -d
