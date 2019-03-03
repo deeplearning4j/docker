8723fdca8d78b85de03b33825d42ed4decc3f040
Serhii Shepel <9946053+sshepel@users.noreply.github.com>
2019-03-03 14:04:05 +0200

    Add Docker manifest files for Skymind CI

--- hadoop-docker/hadoop/push.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker push skymindio-docker-dl4j-hadoop.bintray.io/skymindio/dl4j-hadoop
--- hadoop-docker/pushall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-for i in hadoop namenode datanode resourcemanager nodemanager historyserver spark; do
-    ( cd $i && ./push.sh)
-done
